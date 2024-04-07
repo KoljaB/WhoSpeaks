a936aa4faa78737ffae2b05f246e60bd80c40098
Kolja Beigel <kolja.beigel@web.de>
2024-04-07 11:59:44 +0200

    automatical speaker number detection

--- auto_diarize.py
+++ auto_diarize.py
@@ -1,3 +1,5 @@
+print("Auto Speaker Diarization with Hierarchical Clustering")
+
 import os
 import numpy as np
 import shutil
@@ -11,17 +13,20 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as sch
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
 
 # Setup
 input_directory = 'output_sentences_wav'
 output_directory = 'output_speakers'
 max_sentences = 1000
 minimum_duration = 0.5
-
 data = []
+
 print("Loading TTS model")
 device = torch.device("cuda")
 local_models_path = os.environ.get("COQUI_MODEL_PATH")
+if local_models_path is None:
+    local_models_path = "models"
 checkpoint = os.path.join(local_models_path, "v2.0.2")
 config = load_config((os.path.join(checkpoint, "config.json")))
 tts = setup_tts_model(config)
@@ -36,6 +41,12 @@ tts.load_checkpoint(
 tts.to(device)
 print("TTS model loaded")
 
+
+def get_speaker_embedding(audio_path):
+    _, speaker_embedding = tts.get_conditioning_latents(audio_path=audio_path, gpt_cond_len=30, max_ref_length=60)
+    return speaker_embedding
+
+
 # Create 1D embeddings from sentences
 count = 0
 embeddings = []
@@ -44,7 +55,7 @@ for filename in os.listdir(input_directory):
         y, sr = librosa.load(os.path.join(input_directory, filename))
         if librosa.get_duration(y=y, sr=sr) >= minimum_duration:
             full_path = os.path.join(input_directory, filename)
-            _, speaker_embedding = tts.get_conditioning_latents(audio_path=full_path, gpt_cond_len=30, max_ref_length=60)
+            speaker_embedding = get_speaker_embedding(full_path)
             speaker_embedding_1D = speaker_embedding.view(-1).cpu().detach().numpy()
             embeddings.append(speaker_embedding_1D)
             data.append({'filename': filename, 'speaker_embeds_1D': speaker_embedding_1D})
@@ -59,6 +70,20 @@ embeddings_scaled = scaler.fit_transform(embeddings_array)
 # Hierarchical Clustering
 linked = sch.linkage(embeddings_scaled, method='ward')
 
+# Hierarchical Clustering
+linked = sch.linkage(embeddings_scaled, method='ward')
+
+# Silhouette Analysis for Determining Optimal Number of Clusters
+range_clusters = [i for i in range(2, 11)]  # Examining 2 to 10 clusters
+silhouette_scores = []
+for n_clusters in range_clusters:
+    hc = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward')
+    cluster_labels = hc.fit_predict(embeddings_scaled)
+    silhouette_avg = silhouette_score(embeddings_scaled, cluster_labels)
+    silhouette_scores.append(silhouette_avg)
+
+optimal_cluster_count = range_clusters[silhouette_scores.index(max(silhouette_scores))]
+
 # Plotting the dendrogram
 plt.figure(figsize=(10, 7))
 dendrogram = sch.dendrogram(linked, orientation='top', distance_sort='descending', show_leaf_counts=True)
@@ -67,20 +92,33 @@ plt.title('Dendrogram')
 # Save or show dendrogram
 dendrogram_file = 'dendrogram.png'
 plt.savefig(dendrogram_file)
-print(f"Dendrogram saved as {dendrogram_file}")
+print()
+print(f"The dendrogram image showing the detected speaker clusters was saved as {dendrogram_file}.")
+print()
 
 # Explanation for the user
-print(f"Please inspect the dendrogram plot that has been saved as {dendrogram_file}.")
+print(f"The automatical speaker detection suggested there were {optimal_cluster_count} speakers.")
+print(f"Please verify this by inspecting the dendrogram plot that has been saved as {dendrogram_file}.")
 print("You should look for the longest vertical lines that are not crossed by any horizontal lines.")
 print("These lines suggest a natural separation between different clusters.")
 print("A horizontal 'cut' through these long lines will determine the number of clusters.")
 print("Count the number of vertical lines intersected by an imaginary horizontal line to decide the cluster count.")
 print("This number will be the number of speakers you should input.")
+print()
+print(f"Automatical speaker count suggestion: {optimal_cluster_count} speakers.")
+print()
+print("If you have identified a different number of speakers from the dendragram file, please enter the number.")
+print(f"If you are satisfied with the automatic suggestion of {optimal_cluster_count} speakers, you can press Enter to proceed.")
+print()
 
 # Ask the user for the number of clusters with a retry mechanism if the input fails
 while True:
     try:
-        cluster_count = int(input("Enter the number of speakers (clusters) you have identified: "))
+        input_user = input("Please enter the number of speakers (clusters) you have identified: ")
+        if input_user == "":
+            cluster_count = optimal_cluster_count
+            break
+        cluster_count = int(input_user)
         if cluster_count > 0:
             break
         else:
@@ -109,5 +147,3 @@ for cluster_id in range(cluster_count):
             shutil.copy(source_path, destination_path)
 
 print("Speaker diarization completed with hierarchical clustering.")
-
-
